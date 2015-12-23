55d42234d86afadd8cfc983935a6ee020d8c6356
dongatho <wocamachon@gmail.com>
2015-12-23 22:47:08 +0100

    Assignment 2: Angular Forms: two-way data binding , form validation using Angular and keeping track of the information

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -161,18 +161,23 @@ angular.module('confusionApp',[])
     .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
-            
+            $scope.commentObj = { rating:"", comment:"", author:"", date:"" };
+            $scope.commentObj.rating = 5; 
+        
             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
-                //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
+                $scope.commentObj.date = new Date().toISOString();
                 
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.commentObj);
                 
                 //Step 4: reset your form to pristine
+                $scope.commentForm.$setPristine();
                 
                 //Step 5: reset your JavaScript object that holds your comment
+                $scope.commentObj = { rating:"", comment:"", author:"", date:"" };
+                $scope.commentObj.rating = 5;
             }
     }])
 
