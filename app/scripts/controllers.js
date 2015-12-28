eeb9290b5ed7b2a4048a7b8cad272e915d119889
dongatho <wocamachon@gmail.com>
2015-12-28 16:41:53 +0100

    Angular ngRoute and Single Page Application

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -73,11 +73,11 @@ angular.module('confusionApp')
         };
     }])
 
-    .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-    //.controller('DishDetailController', ['$scope', function($scope) {
+    .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
 
-            $scope.dish= menuFactory.getDish(3);
-                    
+        var dish= menuFactory.getDish(parseInt($routeParams.id,10));
+        $scope.dish = dish;
+    
     }])
 
 
